0670571d89e711cc6d9b2de039635ac106f6e212
mikegorski <mike.gorski96@gmail.com>
2024-05-02 22:39:38 +0200

    [FIX] file formatting

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,9 @@
-from data_pipeline import __version__
 import re
+
+from data_pipeline import __version__
 
 
 def test_version_should_be_of_proper_format():
-    pattern = r'\b\d+\.\d+\.\d+\b'
+    pattern = r"\b\d+\.\d+\.\d+\b"
     match = re.match(pattern, __version__)
     assert match
